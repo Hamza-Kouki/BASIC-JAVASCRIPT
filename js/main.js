f619ed7191180e8101d47b668822cda1a6a0d79e
hamza kouki <hamzakouki33@gmail.com>
2019-02-13 23:45:17 +0100

    second_commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function myFunction_bold() {
+    document.getElementById("abc").style.fontWeight = "1000";
+  }
+  function myFunction_italic() {
+    document.getElementById("abc").style.fontStyle = "italic";
+  }
+  function myFunction_underlined() {
+    document.getElementById("abc").style.textDecoration = "underline overline";
+  }
+  function myFunction_font() {
+    document.getElementById("abc").style.fontWeight = "1000";
+  }
+  function myFunction_size() {
+    document.getElementById("abc").style.fontWeight = "1000";
+  }
+  
