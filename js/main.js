4a6eeac98ebe655a14590086de74c2bc27993227
hamza kouki <hamzakouki33@gmail.com>
2019-02-15 19:29:17 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -13,4 +13,20 @@ function myFunction_bold() {
   function myFunction_size() {
     document.getElementById("abc").style.fontWeight = "1000";
   }
-  
+  ///////////////////////
+  function basic_function(){
+    var select = document.getElementById("selectBox");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    if(selectedValue== "defult_font"){
+        document.getElementById("abc").style.fontFamily = "Comic Sans MS", arial, sans-serif;
+    }
+    else if(selectedValue=="first_font"){
+        document.getElementById("abc").style.fontFamily = "Impact,Charcoal,sans-serif";
+    }
+    else if(selectedValue=="second_font"){
+        document.getElementById("abc").style.fontFamily = "Times New Roman", Times, serif;
+    }
+    else if(selectedValue=="third_font"){
+        document.getElementById("abc").style.fontFamily =  cursive, sans-serif;
+    }
+    }
